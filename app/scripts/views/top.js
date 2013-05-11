06b132f60671ec5f9e39af353fa84f952aa41bc5
Ikuo Terado <eqobar@gmail.com>
2013-05-11 11:54:39 +0900

    Supported generating map id

--- app/scripts/views/top.js
+++ app/scripts/views/top.js
@@ -3,8 +3,9 @@ define([
     'jquery',
     'backbone',
     'underscore',
-    'text!templates/top.html'
-], function ($, Backbone, _, TopViewTemplate) {
+    'text!templates/top.html',
+    'text!templates/link.html'
+], function ($, Backbone, _, TopViewTemplate, LinkTemplate) {
     'use strict';
 
     var TopView = Backbone.View.extend({
@@ -15,6 +16,7 @@ define([
         },
 
         template: _.template(TopViewTemplate),
+        linkTemplate: _.template(LinkTemplate),
 
         initialize: function () {
             // console.log('TopView');
@@ -27,7 +29,19 @@ define([
         },
 
         createMap: function () {
-            console.log('ok');
+            var url = location.href + '#map/' + this.getMapId();
+            // console.log(url);
+
+            $('#link').html(this.linkTemplate({url: url}));
+        },
+
+        getMapId: function () {
+            var id = 'xxxxxxxx'.replace(/x/g, function () {
+                return Math.floor(Math.random() * 36).toString(36);
+            });
+            // console.log(id);
+
+            return id;
         }
     });
 
