925b85bb0b74b7b97a31a491872d933c8837d8c5
Ikuo Terado <eqobar@gmail.com>
2013-05-12 02:36:38 +0900

    Supported nickname

--- app/scripts/views/map.js
+++ app/scripts/views/map.js
@@ -3,20 +3,27 @@ define([
     'jquery',
     'backbone',
     'underscore',
+    'text!templates/nickname.html',
+    'jqueryCookie',
     'http://localhost:9001/pubsub/client.js',
     'async!http://maps.googleapis.com/maps/api/js?sensor=true'
-], function ($, Backbone, _) {
+], function ($, Backbone, _, NicknameTemplate) {
     'use strict';
 
     var MapView = Backbone.View.extend({
-        el: $('#view'),
+        el: $('#map'),
+
+        events: {
+            'keydown #nickname': 'updateNickname'
+        },
 
         id: null,
-
         map: null,
 
+        template: _.template(NicknameTemplate),
+
         defaultMapOptions: {
-            center: new google.maps.LatLng(35.681382,139.766084), // Tokyo station
+            center: new google.maps.LatLng(35.681382, 139.766084), // Tokyo station
             zoom: 15,
             mapTypeId: google.maps.MapTypeId.ROADMAP
         },
@@ -37,11 +44,22 @@ define([
         },
 
         render: function () {
+            this.$el.empty();
+
             this.map = new google.maps.Map((this.$el)[0], this.defaultMapOptions);
+
+            this.$el.append(this.template());
+            $('#nickname').val($.cookie('nickname') || '');
         },
 
         onRecived: function (data) {
             console.log(data);
+        },
+
+        updateNickname: function () {
+            // console.log($('#nickname').val());
+
+            $.cookie('nickname', $('#nickname').val());
         }
     });
 
